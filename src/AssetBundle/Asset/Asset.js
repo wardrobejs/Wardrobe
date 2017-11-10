f46a8229266f724701c8e8b44362e05ef07f4603
Yme-Jan Iedema <yme-jan@iedema.me>
2017-11-10 11:44:14 +0100

    Give credit where credit is due!

--- src/AssetBundle/Asset/Asset.js
+++ src/AssetBundle/Asset/Asset.js
@@ -1,3 +1,10 @@
+/* APEX; Asset Manager                             _______
+ *                                                 ___    |________________  __
+ * Copyright 2017, Harold Iedema                   __  /| |__  __ \  _ \_  |/_/
+ * <harold@iedema.me>                              _  ___ |_  /_/ /  __/_>  <
+ * Licensed under MIT.                             /_/  |_|  .___/\___//_/|_|
+ * ----------------------------------------------------- /_*/
+
 class Asset
 {
     constructor (data)
